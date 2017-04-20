c7cd0cfb70420ab522bcaebb5a3f6d22dcbf8bf4
Aaron Xavier <aarondxavier@gmail.com>
2017-04-20 11:27:35 -0700

    Modified the port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var app = require('./server-config.js');
 
-var port = process.env.PORT || 4568;
-var ip = process.env.IP || 'localhost';
+var port = process.env.PORT || 3000;
+var ip = process.env.IP;
 
-app.listen(port, ip);
+app.listen(port);
 
 console.log('Server now listening on port ' + port);
